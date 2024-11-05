5eeb26a1d48eff594f8f5983e7b10f13813bfb44
Khethiwe-J-Dhlangamandla <kdhlangamandla@gmail.com>
2024-11-05 23:29:19 +0200

    Python week 1 complete.

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# Question 2
+name = input("What is your name? ")
+# print("My name is " + name)
+
+# Question 3
+age = input("How old are you? ")
+# print("I am "+ age + " years old.")
+
+# Question 4
+location = input("Where are you from? ")
+# print("I am from "+ location )
+
+print("Hello " +name+", you are " +age+ " years old and live in " +location+".")
